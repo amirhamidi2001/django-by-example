c6ff67ad991326eb4a0c88c2f3c787df94d41722
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-12-24 12:40:57 +0330

    feat(account): add user registration functionality
    - Add UserRegistrationForm with password confirmation and validation
    - Implement register view to create new users securely
    - Add register URL route
    - Add registration templates (register and register_done)
    - Add link to registration page from login template

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 @login_required
@@ -30,3 +30,20 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+def register(request):
+    """Check if the request method is POST (form submitted)"""
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a user object but do not save it to the database yet
+            new_user = user_form.save(commit=False)
+            # Hash and set the user's password securely
+            new_user.set_password(user_form.cleaned_data["password"])
+            # Save the user object to the database
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
